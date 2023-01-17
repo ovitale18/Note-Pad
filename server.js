3fb4f55d95b64fa03bee1054ba68f0986d028134
Oliver Vitale <ovitale18@gmail.com>
2023-01-17 15:47:15 -0500

    started server,js file to link homepage to note pad

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public")); 
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+);
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`)); 
