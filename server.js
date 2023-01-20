d2e1ee2c05ad352b0d1b81952fd256850bb86cc6
Oliver Vitale <ovitale18@gmail.com>
2023-01-19 23:02:44 -0500

    initial commit

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static("public")); 
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`)); 
